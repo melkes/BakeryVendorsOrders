ed4b9c50faeace833acb3eff19d94c3f077ab0f5
Mike Wilkes <mikewilkes@outlook.com>
2023-03-05 05:32:47 -0800

    moved Find method above ClearAll in Order.cs

--- BakeryVendorsOrders/Models/Order.cs
+++ BakeryVendorsOrders/Models/Order.cs
@@ -26,11 +26,14 @@ namespace BakeryVendorsOrders.Models
     {
       return _instances;
     }
-
+     public static Order Find(int searchId)
+    {
+      return _instances[searchId-1];
+    }
+    
     public static void ClearAll()
     {
       _instances.Clear();
     }
-
   }
 }

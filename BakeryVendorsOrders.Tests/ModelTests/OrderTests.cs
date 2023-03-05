202abdbdfeda1299751864a9c69ea49cd14f9d8b
Mike Wilkes <mikewilkes@outlook.com>
2023-03-05 04:26:14 -0800

    order list test passing

--- BakeryVendorsOrders.Tests/ModelTests/OrderTests.cs
+++ BakeryVendorsOrders.Tests/ModelTests/OrderTests.cs
@@ -1,43 +1,47 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using System.Collections.Generic;
-// using BakeryVendorsOrders.Models;
-// using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using BakeryVendorsOrders.Models;
+using System;
 
-// namespace BakeryVendorsOrders.Tests
-// {
-//   [TestClass]
+namespace BakeryVendorsOrders.Tests
+{
+  [TestClass]
 
-//   public class OrderTests
-//   {
-//     [TestMethod]
-//     public void OrderConstructor_CreatesInstanceOfOrder_Order()
-//     {
-//       Order newOrder = new Order("test order");
-//       Assert.AreEqual(typeof(Order), newOrder.GetType());
-//     }
+  public class OrderTests
+  {
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order("test order", "test description", 10, "test date");
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
 
-//     [TestMethod]
-//     public void GetName_ReturnsName_String()
-//     {
-//       //Arrange
-//       string name = "36 doughnuts";
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      //Arrange
+      string name = "36 doughnuts";
+      string description = "test description";
 
-//       //Act
-//       Vendor newVendor = new Vendor(name);
-//       string result = newVendor.Name;
+      //Act
+      Vendor newVendor = new Vendor(name, description);
+      string result = newVendor.Name;
 
-//       //Assert
-//       Assert.AreEqual(name, result);
-//     }
+      //Assert
+      Assert.AreEqual(name, result);
+    }
 
 
-//       [TestMethod]
-//     public void GetAll_ReturnsAListOf_Order()
-//     {
-//       //create a couple vendors
-//       List<Order> vendorList = Vendor.ClearAll();
-//       List<Order> emptyList = new List<Vendor>{}
-//       Assert.AreEqual(vendorList,emptyList)
-//     }
-//   }
-// }
+      [TestMethod]
+    public void GetAll_ReturnsAListOf_Order()
+    {
+      Order.ClearAll();
+      Order orderTest = new Order("test order", "test description", 10, "test date");
+      List<Order> testingList = new List<Order>{orderTest};
+      List<Order> orderList = Order.GetAll();
+
+      CollectionAssert.AreEqual(orderList, testingList);
+    }
+
+  }
+}
